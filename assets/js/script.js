771245ac7520e6a4dffdbd8892f2ba4ba42b05da
Yasmin Castillo <yasmin_c@outlook.com>
2023-05-21 13:14:22 -0500

    added buttom

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
 console.log("File is Linked");
+var button = document.getElementById("button");
+button.addEventListener("click",startQuiz)
+
+function startQuiz() {
+    console.log("Starting Quiz :D ");
+    var testing = document.querySelector(".test");
+    testing.classList.remove("hide");
+    setInterval(displayHello, 1000);
+}
+
+
+var count = 59;
+function displayHello() {
+ var timer= document.getElementById("timer");
+ 
+ timer.innerHTML =count;
+ count--;
+}
